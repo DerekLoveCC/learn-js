6f40afed9f56f45dda6497a94849e7b0e92313a2
Derek <zhaomingbo1010@126.com>
2024-10-29 10:28:17 +0800

    re-org

---
+++ code/switch.js
@@ -0,0 +1,19 @@
+let x = 10;
+
+switch (x) {
+  case 9:
+    console.log("9");
+    break;
+  case 10:
+    console.log("10");
+  case 11:
+    console.log("11");
+  case 12:
+    console.log("12");
+    break;
+  case 13:
+    console.log("13");
+    break;
+  default:
+    console.log("default");
+}
